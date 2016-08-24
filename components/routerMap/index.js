3126b0e606477d1f8e5706875b14339bf296e63c
otarim <otarim.com@gmail.com>
2016-08-24 23:16:54 +0800

    add routerMap module

---
+++ components/routerMap/index.js
@@ -0,0 +1,54 @@
+'use strict'
+var util = require('util')
+var routerMap = getConfig().routerMap
+
+function makeReg(str) {
+    var vars = []
+    str = str.replace(/\?/g,'\\?')
+    str = str.replace(/:[A-Za-z]*/g,function(key) {
+        vars.push(key.slice(1))
+        return '([^\/]*)'
+    })
+    return {
+        reg: new RegExp(str),
+        vars
+    }
+}
+
+function replaceParams(redirectUrl, vars) {
+    Object.keys(vars).forEach(function(key) {
+        redirectUrl = redirectUrl.replace('$' + key, vars[key])
+    })
+    return redirectUrl
+}
+
+function getRouterVars(vars, matched) {
+    var ret = {}
+    vars.forEach(function(key, index) {
+        ret[key] = matched[index]
+    })
+    return ret
+}
+
+module.exports = function* (next) {
+    var req = this.request,
+        url = req.url
+    for (var i in routerMap) {
+        if (routerMap.hasOwnProperty(i)) {
+            var result = makeReg(i),
+                reg = result.reg,
+                vars = result.vars,
+                matched = reg.exec(url)
+            if (matched) {
+                if (util.isFunction(routerMap[i])) {
+                    routerMap[i].apply(this, matched.slice(1))
+                } else if (util.isString(routerMap[i])) {
+                    var vars = getRouterVars(vars, matched.slice(1))
+                    this.redirect(replaceParams(routerMap[i], vars))
+                }
+                break
+            }
+        }
+    }
+    yield next
+}
